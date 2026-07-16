fde6058e81fb82f95d7d4e4878833e4a258a8b5e
anilakurathi998 <anilkumarakurathi998@gmail.com>
2026-07-16 14:26:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+new Typed("#typing",{
+
+strings:[
+
+"Senior Data Analyst",
+
+"Power BI Developer",
+
+"Microsoft Fabric Engineer",
+
+"Azure Data Factory Developer",
+
+"SQL Developer",
+
+"Python Developer"
+
+],
+
+typeSpeed:70,
+
+backSpeed:40,
+
+loop:true
+
+});
+particlesJS("particles-js",{
+
+particles:{
+
+number:{
+value:80
+},
+
+color:{
+value:"#38bdf8"
+},
+
+shape:{
+type:"circle"
+},
+
+opacity:{
+value:.4
+},
+
+size:{
+value:3
+},
+
+move:{
+enable:true,
+speed:2
+}
+
+}
+
+});
+const topBtn=document.getElementById("topBtn");
+
+window.onscroll=function(){
+
+if(document.documentElement.scrollTop>300){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+}
+
+topBtn.onclick=function(){
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+}
+const sections=document.querySelectorAll("section");
+
+const navLinks=document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll",()=>{
+
+let current="";
+
+sections.forEach(section=>{
+
+const sectionTop=section.offsetTop-150;
+
+if(pageYOffset>=sectionTop){
+
+current=section.getAttribute("id");
+
+}
+
+});
+
+navLinks.forEach(link=>{
+
+link.classList.remove("active");
+
+if(link.getAttribute("href")==="#"+current){
+
+link.classList.add("active");
+
+}
+
+});
+
+});
+const counters=document.querySelectorAll(".counter");
+
+counters.forEach(counter=>{
+
+const update=()=>{
+
+const target=+counter.dataset.target;
+
+const current=+counter.innerText;
+
+const increment=target/80;
+
+if(current<target){
+
+counter.innerText=(current+increment).toFixed(1);
+
+setTimeout(update,20);
+
+}else{
+
+counter.innerText=target+"+";
+
+}
+
+}
+
+update();
+
+});
+const themeBtn=document.getElementById("themeBtn");
+
+themeBtn.onclick=function(){
+
+document.body.classList.toggle("light");
+
+}
